2250b3f1e2cb0d7fbb10c9c18b82abcde8b1f934
Roelof Roos <roelofhylkeroos@gmail.com>
2014-05-06 10:46:41 +0200

    Made it a project =D Signed-off-by:Roelof Roos <roelofhylkeroos@gmail.com>

---
+++ src/kta02/warehouse/SimLauncher.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kta02.warehouse;
+
+/**
+ *
+ * @author Roelof
+ */
+public class SimLauncher {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("KTA02");
+        System.out.println("Bin-Packing Problem Simulator");
+        
+    }
+    
+}
