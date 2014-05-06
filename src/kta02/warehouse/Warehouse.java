769ddf0bf89f038f17216130f3066b49ad488943
Huib <Huib@145.44.51.109>
2014-05-06 15:12:45 +0200

    SerialCom class aangemaakt

---
+++ src/kta02/warehouse/Warehouse.java
@@ -0,0 +1,18 @@
+package kta02.warehouse;
+
+/**
+ *
+ * @author Huib
+ */
+public class Warehouse
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("KTA02");
+        System.out.println("Bin-Packing Problem Simulator");
+
+    }
+
+}
